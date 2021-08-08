7b2301c94431e70b59ab89e46714c9c62f568d16
doursat <doursat@yahoo.com>
2021-08-08 20:07:51 +0200

    Updates

---
+++ precache-manifest.e686d434dcf80be2ec87ad254ae91dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccab2ae9c85715355233091db1ff088",
+    "url": "/voting2/index.html"
+  },
+  {
+    "revision": "63026d9c564bec98a930",
+    "url": "/voting2/static/css/2.32e6ede2.chunk.css"
+  },
+  {
+    "revision": "d09e302d391f1ba3df3e",
+    "url": "/voting2/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "63026d9c564bec98a930",
+    "url": "/voting2/static/js/2.3a5d3cac.chunk.js"
+  },
+  {
+    "revision": "d09e302d391f1ba3df3e",
+    "url": "/voting2/static/js/main.3a797951.chunk.js"
+  },
+  {
+    "revision": "4b8980531bb35b526abc",
+    "url": "/voting2/static/js/runtime-main.74737a20.js"
+  }
+]);
